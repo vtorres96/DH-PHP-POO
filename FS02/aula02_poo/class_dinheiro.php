43d08497e83a831d03df65a48a0bc5ca126b72d4
vtorres96 <victorcdc96@gmail.com>
2019-09-01 19:07:44 -0300

    adding second  class in class FS02

---
+++ FS02/aula02_poo/class_dinheiro.php
@@ -0,0 +1,52 @@
+<?php
+
+  class Cotacao{
+
+  public $dolarProp;
+  public $cotacaoProp;
+
+  public function valores(){
+      $resultado = ($this->dolarProp * $this->cotacaoProp);
+      return ($resultado);
+  }
+}
+
+require 'Dinheiro.php';
+require 'Cotacao.php';
+
+$dolar = new Cotacao();
+$dolar->valor = 10;
+$dolar->moeda = 'Dólar';
+
+$cotacao = new Cotacao($dolar, );
+
+echo "Valor em reais " . $cotacao->converter() . " valor em dólar " . $dolar->
+
+ ?>
+
+
+
+
+<?php
+
+class Dinheiro(){
+  public $valor;
+  public $moeda;
+}
+
+class Cotacao(){
+
+  public $dinheiro;
+  public $valorCotacao;
+
+  public function __construct($dinheiroConverter){
+    $this->dinheiro = $dinheiroConverter;
+    $this->valorCotacao = $valorCotacao;
+  }
+
+  public function converter(){
+    $this->dinheiro->valor * $this->$valorCotacao;
+  }
+}
+
+ ?>
